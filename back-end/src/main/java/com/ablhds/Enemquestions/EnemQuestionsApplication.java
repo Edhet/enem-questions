9eca2eefd090e482f4dbdb74b6d446c325f863c6
Eduardo P. <edtvports@gmail.com>
2024-03-30 15:10:22 -0300

    Initialized monorepo

---
+++ back-end/src/main/java/com/ablhds/Enemquestions/EnemQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.ablhds.Enemquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemQuestionsApplication.class, args);
+	}
+
+}
