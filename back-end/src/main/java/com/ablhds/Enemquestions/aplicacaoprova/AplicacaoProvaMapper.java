74879bc395aae36e2ebd8715326e58491b0ec1d4
Fernando Gama <ogfernandog@gmail.com>
2024-04-29 17:52:51 -0300

    Adição do método dtoToEntity na classe AplicacaoProvaMapper, falta definir como vai ser feita a busca pela prova

--- back-end/src/main/java/com/ablhds/Enemquestions/aplicacaoprova/AplicacaoProvaMapper.java
+++ back-end/src/main/java/com/ablhds/Enemquestions/aplicacaoprova/AplicacaoProvaMapper.java
@@ -1,9 +1,13 @@
 package com.ablhds.Enemquestions.aplicacaoprova;
 
+import com.ablhds.Enemquestions.metricas.Metricas;
 import com.ablhds.Enemquestions.metricas.MetricasMapper;
+import com.ablhds.Enemquestions.prova.Prova;
+import com.ablhds.Enemquestions.resposta.Resposta;
 import com.ablhds.Enemquestions.resposta.RespostaDto;
 import com.ablhds.Enemquestions.resposta.RespostaMapper;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +33,33 @@ public final class AplicacaoProvaMapper {
     }
 
     public static AplicacaoProva dtoToEntity(AplicacaoProvaDto aplicacaoProvaDto) {
-        return null;
+
+        EstadoAplicacaoProva estadoAplicacaoProva = EstadoAplicacaoProva.valueOf(aplicacaoProvaDto.estadoAplicacaoProva());
+
+        List<Resposta> respostas = aplicacaoProvaDto.respostas()
+                .stream()
+                .map(RespostaMapper::dtoToEntity)
+                .collect(Collectors.toList());
+
+        //todo Vamos buscar a prova pelo id?
+        Prova prova = new Prova();
+        prova.setId(aplicacaoProvaDto.id());
+
+        Metricas metricas = MetricasMapper.dtoToEntity(aplicacaoProvaDto.metricas());
+
+        LocalDateTime tempoInicio = LocalDateTime.parse(aplicacaoProvaDto.tempoInicioDeAplicacao());
+        LocalDateTime tempoFinal = LocalDateTime.parse(aplicacaoProvaDto.tempoFimDeAplicacao());
+
+        AplicacaoProva aplicacaoProva = new AplicacaoProva();
+
+        aplicacaoProva.setId(aplicacaoProvaDto.id());
+        aplicacaoProva.setEstadoAplicacaoProva(estadoAplicacaoProva);
+        aplicacaoProva.setTempoInicioDeAplicacao(tempoInicio);
+        aplicacaoProva.setTempoFimDeAplicacao(tempoFinal);
+        aplicacaoProva.setRespostas(respostas);
+        aplicacaoProva.setMetricas(metricas);
+        aplicacaoProva.setProva(prova);
+        return aplicacaoProva;
     }
 
 
