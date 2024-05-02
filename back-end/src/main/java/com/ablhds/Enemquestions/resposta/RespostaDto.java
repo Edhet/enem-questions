ed287b703dda45a364418c0e967faf8015817c73
Fernando Gama <ogfernandog@gmail>
2024-05-01 22:49:07 -0300

    Atributos da Classe ResostaDto mudados para o tipo de seus respectivos dtos

--- back-end/src/main/java/com/ablhds/Enemquestions/resposta/RespostaDto.java
+++ back-end/src/main/java/com/ablhds/Enemquestions/resposta/RespostaDto.java
@@ -1,9 +1,13 @@
 package com.ablhds.Enemquestions.resposta;
 
+import com.ablhds.Enemquestions.aplicacaoprova.AplicacaoProvaDto;
+import com.ablhds.Enemquestions.opcao.OpcaoDto;
+import com.ablhds.Enemquestions.questao.QuestaoDto;
+
 public record RespostaDto(
         long id,
-        long idQuestao,
-        long idOpcaoEscolhida,
-        long idAplicacaoProva
+        QuestaoDto questao,
+        OpcaoDto opcaoEscolhida,
+        AplicacaoProvaDto aplicacaoProvaDto
 ) {
 }
