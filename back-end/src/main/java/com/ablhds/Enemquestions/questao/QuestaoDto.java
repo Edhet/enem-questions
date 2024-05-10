0302389a4b8342083f2cc056fee001083d22602c
Fernando Gama <ogfernandog@gmail>
2024-05-10 16:39:21 -0300

    atributo enunciado adicionado na classe Questao, QuestaoMapper,QuestaoDto

--- back-end/src/main/java/com/ablhds/Enemquestions/questao/QuestaoDto.java
+++ back-end/src/main/java/com/ablhds/Enemquestions/questao/QuestaoDto.java
@@ -7,6 +7,7 @@ import java.util.List;
 public record QuestaoDto(
         long id,
         int numeroQuestao,
-        List<OpcaoDto> opcoes
+        List<OpcaoDto> opcoes,
+        String enunciado
 ) {
 }
