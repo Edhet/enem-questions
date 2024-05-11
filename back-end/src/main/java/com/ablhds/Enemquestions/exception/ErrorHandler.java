63377a2b68e24325f827664694cd8cd63190b55c
Eduardo P. <edtvports@gmail.com>
2024-05-11 09:37:11 -0300

    Backup do estado da branch

---
+++ back-end/src/main/java/com/ablhds/Enemquestions/exception/ErrorHandler.java
@@ -0,0 +1,25 @@
+package com.ablhds.Enemquestions.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> customExceptionsHandler(Exception e) {
+        HttpStatus status = e.getClass().getAnnotation(ResponseStatus.class).value();
+
+        ErrorDto errorResponse = new ErrorDto(
+                status.value(),
+                status.getReasonPhrase(),
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(status.value()).body(errorResponse);
+    }
+}
--- back-end/src/main/java/com/ablhds/Enemquestions/usuario/UsuarioController.java
+++ back-end/src/main/java/com/ablhds/Enemquestions/usuario/UsuarioController.java
@@ -1,12 +1,17 @@
 package com.ablhds.Enemquestions.usuario;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/usuario")
+@RequestMapping("/v1/usuario")
 @AllArgsConstructor
 public class UsuarioController {
 
+    @GetMapping()
+    public boolean check() {
+        return true;
+    }
 }
