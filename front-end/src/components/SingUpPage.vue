4bcbca688e6c14ae100c7335506ed5b6da4b1129
Miguel Cordeiro <miguelcordeiro831@gmail.com>
2024-05-22 18:54:22 -0300

    Alterações no funcionamento de ocultar senha, funcionamento do botão e mensagem que encaminha à página de login

--- front-end/src/components/SingUpPage.vue
+++ front-end/src/components/SingUpPage.vue
@@ -4,12 +4,15 @@
             <div class="flex w-1/2 bg-white rounded-lg mx-auto shadow-xl overflow-hidden">
                 <div class="w-full py-5 px-5">
                     <h2 class="text-3xl mb-4">
-                        Registrar Conta
+                        Cadastrar Conta
                     </h2>
                     <p class="mb-4">
-                        Crie a sua conta para continuar.
+                        Já está cadastrado? Acesse a sua conta 
+                        <a href="COLOCAR LINK PARA LOGIN" class="font-semibold text-blue-800 underline">
+                            AQUI.
+                        </a>
                     </p>
-                    <form action="#">
+                    <form>
                         <div class="grid grid-cols-2 gap-5">
                             <input type="text" placeholder="Nome" class="border border-gray-400 py-1 px-2">
                             <input type="text" placeholder="Sobrenome" class="border border-gray-400 py-1 px-2">
@@ -19,32 +22,32 @@
                         </div>
                         <div class="mt-5 relative flex items-center">
                             <input v-model="senha" :type="mostrarSenha ? 'text' : 'password'" placeholder="Senha" class="border border-gray-400 py-1 px-2 w-full pr-10">
-                            <button @click="mostrarSenha = !mostrarSenha" class="absolute right-0 px-1 h-6 rounded-l-lg rounded-r-lg" :class="{ 'bg-blue-600 text-white': mostrarSenha, 'bg-blue-800 text-white': !mostrarSenha }" style="font-size: 0.75rem; margin-right: 5px;">
+                            <button @click="mostrarSenha = !mostrarSenha; $event.preventDefault()" class="absolute right-0 px-1 h-6 rounded-l-lg rounded-r-lg" :class="{ 'bg-blue-600 text-white': mostrarSenha, 'bg-blue-800 text-white': !mostrarSenha }" style="font-size: 0.75rem; margin-right: 5px;">
                                 {{ mostrarSenha ? 'Ocultar' : 'Mostrar' }}
                             </button>
                         </div>
                         <div class="mt-5 relative flex items-center">
                             <input v-model="confirmarSenha" :type="mostrarConfirmarSenha ? 'text' : 'password'" placeholder="Confirmar Senha" class="border border-gray-400 py-1 px-2 w-full pr-10">
-                            <button @click="mostrarConfirmarSenha = !mostrarConfirmarSenha" class="absolute right-0 px-1 h-6 rounded-l-lg rounded-r-lg" :class="{ 'bg-blue-600 text-white': mostrarConfirmarSenha, 'bg-blue-800 text-white': !mostrarConfirmarSenha }" style="font-size: 0.75rem; margin-right: 5px;">
+                            <button @click="mostrarConfirmarSenha = !mostrarConfirmarSenha; $event.preventDefault()" class="absolute right-0 px-1 h-6 rounded-l-lg rounded-r-lg" :class="{ 'bg-blue-600 text-white': mostrarConfirmarSenha, 'bg-blue-800 text-white': !mostrarConfirmarSenha }" style="font-size: 0.75rem; margin-right: 5px;">
                                 {{ mostrarConfirmarSenha ? 'Ocultar' : 'Mostrar' }}
                             </button>
                         </div>
                         <div class="mt-5">
-                            <input type="checkbox" class="border border-gray-400">
+                            <input type="checkbox" v-model="aceitouTermos" class="border border-gray-400">
                             <span>
                                 Eu aceito os 
-                                <a href="https://policies.google.com/terms?hl=pt-BR" class="text-blue-600 font-semibold">
+                                <a href="https://policies.google.com/terms?hl=pt-BR" target="_blank" class="text-blue-600 font-semibold">
                                     Termos de Uso
                                 </a> 
                                 e 
-                                <a href="https://policies.google.com/privacy?hl=pt-BR" class="text-blue-600 font-semibold">
+                                <a href="https://policies.google.com/privacy?hl=pt-BR" target="_blank" class="text-blue-600 font-semibold">
                                     Políticas de Privacidade
                                 </a>
                                 .
                             </span>
                         </div>
                         <div class="mt-5">
-                            <button class="w-full bg-blue-800 py-3 text-center text-white">
+                            <button class="w-full py-3 text-center text-white rounded-md" :class="{ 'bg-blue-400': !aceitouTermos, 'bg-blue-800': aceitouTermos }" :disabled="!aceitouTermos">
                                 Criar Conta
                             </button>
                         </div>
@@ -63,7 +66,8 @@ export default {
             senha: '',
             confirmarSenha: '',
             mostrarSenha: false,
-            mostrarConfirmarSenha: false
+            mostrarConfirmarSenha: false,
+            aceitouTermos: false
         };
     }
 };
