0b6ae869158b8c570112e4b5409791b27c131ae2
renan-alves <renan.c.alves@edu.unirio.br>
2024-05-23 23:35:49 -0300

    "createMemoryHistory" trocado para "createWebHistory"

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import SingUpPage from './components/SingUpPage.vue'
 import LoginPage from './components/LoginPage.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
