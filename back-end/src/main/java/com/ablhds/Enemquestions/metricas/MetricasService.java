4f1884380932944a6cdc0285df80e68d50432a99
Fernando Gama <ogfernandog@gmail>
2024-06-03 20:35:48 -0300

    Metodo para calcular numero de acertos

--- back-end/src/main/java/com/ablhds/Enemquestions/metricas/MetricasService.java
+++ back-end/src/main/java/com/ablhds/Enemquestions/metricas/MetricasService.java
@@ -1,7 +1,13 @@
 package com.ablhds.Enemquestions.metricas;
 
 import com.ablhds.Enemquestions.aplicacaoprova.AplicacaoProva;
+import com.ablhds.Enemquestions.opcao.Opcao;
+import com.ablhds.Enemquestions.prova.Prova;
+import com.ablhds.Enemquestions.questao.Questao;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MetricasService // falta colocar a aplicacao prova
@@ -12,22 +18,31 @@ public class MetricasService // falta colocar a aplicacao prova
 
         // como resolver isso
     }
+
     public Long calcularSegundosTotais(AplicacaoProva aplicacaoProva) {
         int SegundoTotais = Integer.parseInt(aplicacaoProva.getTempoFimDeAplicacao() - aplicacaoProva.getTempoInicioDeAplicacao());
     }
 
-    public Long calcularTempoMedioQuestao(AplicacaoProva aplicacaoProva){
-        return calcularSegundosTotais(aplicacaoProva)
-                / aplicacaoProva
+    public Long calcularTempoMedioQuestao(AplicacaoProva aplicacaoProva) {
+        return calcularSegundosTotais(aplicacaoProva) / aplicacaoProva
                 .getProva()
                 .getQuestoes()
                 .size();
     }
 
-    public Long CalcularNumeroAcertosnumeroAcertos (){
-        // comparar e usar programacao funcional e incrementar um quando a resposta for igual a questao
-    }
+    public Long CalcularNumeroAcertos(AplicacaoProva aplicacaoProva) {
 
+        return aplicacaoProva.getRespostas()
+                .stream()
+                .map(a -> a.getOpcaoEscolhida().getId())
+                .filter(aLong -> aLong.equals(
+                                aplicacaoProva.getProva().getQuestoes()
+                                        .stream()
+                                        .map(Questao::getOpcaoCorreta)
+                                        .map(Opcao::getId)
+                        )
+                ).count();
+    }
 
 
 }
