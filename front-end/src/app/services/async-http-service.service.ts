bffdc79cf8c1d335e53e0356b8480d104c54de04
Eduardo P. <edtvports@gmail.com>
2024-08-09 17:34:56 -0300

    Mudança de nome do serviço

--- front-end/src/app/services/async-http-service.service.ts
+++ front-end/src/app/services/async-http-service.service.ts
@@ -5,7 +5,7 @@ import { firstValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class HttpServiceService {
+export class AsyncHttpServiceService {
 
   constructor(private httpClient: HttpClient) { }
 
