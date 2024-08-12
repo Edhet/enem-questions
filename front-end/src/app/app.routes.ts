e705ddea7e5f5c9b93909d0b3d9675b00657aa22
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-08-11 22:12:10 -0300

    rota da página 'profile' adicionada

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'profile', component: ProfileComponent }
 ];
