34f8bb6cda97c3a40d810a15790b1158241b62d8
Miguel <140199292+MiguelCSLima@users.noreply.github.com>
2024-08-20 12:18:48 -0300

    Correções de espaçamento

--- test/script_k6/scenarios/Post-Get-Prova.js
+++ test/script_k6/scenarios/Post-Get-Prova.js
@@ -1,20 +1,16 @@
-import http from "k6/http";  // biblioteca com os metodos http
-import { sleep } from "k6"; // serve para dar um descanso
-import { Trend, Rate, Counter } from "k6/metrics"; // serve para configurar as metricas
+import http from "k6/http";
+import { sleep } from "k6";
+import { Trend, Rate, Counter } from "k6/metrics";
 import { check, fail } from "k6";
-
 
 const urllogin = 'http://localhost/api/v1/auth/login';
 const urlPostProva = 'http://localhost/api//v1/prova/nova';
 const urlget = 'http://localhost/api//v1/prova/all';
 
-
 export default function () {
-    // Primeiro, faça uma requisição de login para obter o token
 
     let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhYmxoZHMuY29tIiwiaWF0IjoxNzI0MTEwMTEwLCJleHAiOjE3MjQxOTY1MTB9.8Jt90mbNJ1t2iWSWl7qLFzoHgWWjiOXtkgp0UwxpgO7TvXOGcrrFm1OXEoeO2_oJ6r5M-qdcI-aGEw5HBvFxFQ"
 
-    //header
     let authHeaders = {
         headers: {
             'Content-Type': 'application/json',
@@ -22,9 +18,6 @@ export default function () {
         },
     };
 
-    // // pegou o token de login
-
-    // arquivo com a data da prova
     let data = {
         "nome": "Nova prova " + Math.random(20),
         "areaProva": "EXATAS",
@@ -54,9 +47,4 @@ export default function () {
     let res = http.post(urlPostProva, JSON.stringify(data), authHeaders);
 
     let resget = http.get(urlget,authHeaders);
-
-
-    
-
-
 }
