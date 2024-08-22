49c7b15b4ee08a9b3af607e1dabbb67ef9c6f113
Miguel Cordeiro <miguelcordeiro831@gmail.com>
2024-08-21 21:12:57 -0300

    Criando histórico e métricas do usuário

---
+++ front-end/src/app/metrics/metrics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metrics',
+  standalone: true,
+  imports: [],
+  templateUrl: './metrics.component.html',
+  styleUrl: './metrics.component.css'
+})
+export class MetricsComponent {
+
+}
