9f144db52d9ab9f51d43d0c07bffbbde8a94c13e
Miguel <miguelcordeiro831@gmail.com>
2024-08-23 19:46:25 -0300

    Inserindo arquivo novamente

---
+++ front-end/src/app/services/async-http-service.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AsyncHttpServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getRequest<Response>(url: string): Promise<Response> {
+    return firstValueFrom(this.httpClient.get<Response>(url));
+  }
+
+  public postRequest<Response>(url: string, body: any): Promise<Response> {
+    return firstValueFrom(this.httpClient.post<Response>(url, body));
+  }
+
+  public deleteRequest<Response>(url: string): Promise<Response> {
+    return firstValueFrom(this.httpClient.delete<Response>(url));
+  }
+
+  public putRequest<Response>(url: string, body: any): Promise<Response> {
+    return firstValueFrom(this.httpClient.put<Response>(url, body));
+  }
+
+  public patchRequest<Response>(url: string, body?: any): Promise<Response> {
+    return firstValueFrom(this.httpClient.patch<Response>(url, body));
+  }
+}
