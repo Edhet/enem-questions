d7fe0f2fbac213f0363fa896d480f2e8a07754bb
Miguel <140199292+MiguelCSLima@users.noreply.github.com>
2024-08-23 20:04:06 -0300

    Removendo caminho inexistente

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { MetricsComponent } from './metrics/metrics.component';
 import { HistoryComponent } from './history/history.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -11,7 +10,6 @@ export const routes: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'metrics', component: MetricsComponent },
   { path: 'history', component: HistoryComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
