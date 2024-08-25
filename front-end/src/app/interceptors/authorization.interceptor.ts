92f6457b145aa40263785ca4a80eed1c425f979f
Eduardo P. <edtvports@gmail.com>
2024-08-25 11:37:14 -0300

    removido log no interceptor

--- front-end/src/app/interceptors/authorization.interceptor.ts
+++ front-end/src/app/interceptors/authorization.interceptor.ts
@@ -3,8 +3,6 @@ import { inject, Injectable } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log("INTERCEPTED");
-  
   const authToken = inject(AuthService).getAuthToken();
   const newReq = req.clone({
     headers: req.headers.append("Authorization", `Bearer ${authToken}`)
