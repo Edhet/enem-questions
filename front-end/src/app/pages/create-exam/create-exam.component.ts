8cc5fe9db687d13604d7495e540b70bfb0b12199
Miguel <140199292+MiguelCSLima@users.noreply.github.com>
2024-08-25 21:25:53 -0300

    Alterando TS de create-exam

--- front-end/src/app/pages/create-exam/create-exam.component.ts
+++ front-end/src/app/pages/create-exam/create-exam.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { DiaDeProva } from '../../models/dia-de-prova.model';
 import { AreaProva } from '../../models/area-prova.model';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'app-create-exam',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './create-exam.component.html',
   styleUrl: './create-exam.component.css'
 })
 export class CreateExamComponent {
+  // EXEMPLO
   examOptions = {
-    ano: new Date().getFullYear(),
-    diaDeProva: DiaDeProva.PRIMEIRO,
-    areaProva: AreaProva.EXATAS
+    ano: new Date().getFullYear(), // Ano atual como padrão
+    diaDeProva: 'PRIMEIRO', // Definindo 'PRIMEIRO' como padrão
+    corDaProva: 'AMARELA' // Definindo 'AMARELA' como padrão
   };
+
+  // examOptions = {
+  //   ano: new Date().getFullYear(),
+  //   diaDeProva: DiaDeProva.PRIMEIRO,
+  //   areaProva: AreaProva.EXATAS
+  // };
 
   // constructor(
   //   private asyncHttpService: AsyncHttpServiceService,
