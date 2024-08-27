51a2aab6010e8546f53780f7d4bed312d1ab7083
Miguel <miguelcordeiro831@gmail.com>
2024-08-26 21:52:18 -0300

    Alterações visuais nos botões, Criação visual inicial de question-view, Alterações visuais em profile e history e Alteração na nav-bar

--- front-end/src/app/pages/profile/profile.component.ts
+++ front-end/src/app/pages/profile/profile.component.ts
@@ -11,8 +11,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  personalInfoForm!: FormGroup;
-  isEditing = false;
   profileImgUrl = 'https://i.pinimg.com/564x/69/76/39/6976397d27eed9f81a8ee9f806ccf492.jpg';
   selectedFileName: string | null = null;
 
@@ -27,51 +25,14 @@ export class ProfileComponent implements OnInit {
     birthDate: '1990-10-10'
   };
 
-  private originalUserData = { ...this.user };
-  private originalProfileImgUrl = this.profileImgUrl;
-
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.personalInfoForm = this.fb.group({
-      fullName: [{ value: this.user.fullName, disabled: true }, Validators.required],
-      address: [{ value: this.user.address, disabled: true }, Validators.required],
-      phone: [{ value: this.user.phone, disabled: true }, Validators.required],
-      birthDate: [{ value: this.user.birthDate, disabled: true }, Validators.required]
-    });
-  }
-
-  editProfile(): void {
-    this.isEditing = true;
-    this.personalInfoForm.enable();
-  }
-
-  saveChanges(): void {
-    if (this.personalInfoForm.valid) {
-      Object.assign(this.user, this.personalInfoForm.value);
-      this.isEditing = false;
-      this.personalInfoForm.disable();
-    }
-  }
-
-  cancelEdit(): void {
-    this.personalInfoForm.patchValue(this.originalUserData);
-    this.profileImgUrl = this.originalProfileImgUrl;
-    this.isEditing = false;
-    this.personalInfoForm.disable();
+    // No form initialization needed
   }
 
   onFileChange(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.profileImgUrl = e.target.result;
-      };
-      reader.readAsDataURL(file);
-      this.selectedFileName = file.name;
-    }
+    // Remove the file change handler
   }
 
   confirmDeleteAccount(): void {
--- front-end/src/app/pages/question-view/question-view.component.ts
+++ front-end/src/app/pages/question-view/question-view.component.ts
@@ -1,12 +1,90 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-question-view',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,  CommonModule],
   templateUrl: './question-view.component.html',
   styleUrl: './question-view.component.css'
 })
 export class QuestionViewComponent {
+  currentQuestionIndex = 0;
+  selectedOptions: { [key: number]: string | null } = {}; // Armazena a seleção para cada pergunta
 
+  // EXEMPLO DE COMO SERIA
+
+  questions = [
+    {
+      number: 1,
+      content: 'Conteúdo da questão 1',
+      options: ['Opção A', 'Opção B', 'Opção C', 'Opção D', 'Opção E'],
+      image: 'https://s2-techtudo.glbimg.com/JsE244mucjKWLYtNgeiDyfVYlJQ=/0x129:1024x952/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/i/ME2AxRRoygUyFPCDe0jQ/3.png'
+    },
+    {
+      number: 2,
+      content: 'Conteúdo da questão 2',
+      options: ['Opção A', 'Opção B', 'Opção C', 'Opção D', 'Opção E']
+    },
+    {
+      number: 3,
+      content: 'Conteúdo da questão 3',
+      options: ['Opção A', 'Opção B', 'Opção C', 'Opção D', 'Opção E'],
+      image: 'https://dm0fehhuxv6f6.cloudfront.net/wp-content/uploads/2023/04/30035515/bing-panda-600x600.jpg'
+    },
+    {
+      number: 4,
+      content: 'Conteúdo da questão 4',
+      options: ['Opção A', 'Opção B', 'Opção C', 'Opção D', 'Opção E']
+    },
+    {
+      number: 5,
+      content: 'Conteúdo da questão 5',
+      options: ['Opção A', 'Opção B', 'Opção C', 'Opção D', 'Opção E']
+    }
+  ];
+
+  constructor() {
+    this.loadSelections();
+  }
+
+  get selectedOption(): string | null {
+    return this.selectedOptions[this.currentQuestionIndex] || null;
+  }
+
+  set selectedOption(option: string | null) {
+    this.selectedOptions[this.currentQuestionIndex] = option;
+  }
+
+  nextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex++;
+      this.loadSelections();
+    }
+  }
+
+  previousQuestion() {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+      this.loadSelections();
+    }
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
+
+  pauseExam() {
+    // Lógica para pausar a prova e redirecionar para a home
+  }
+
+  finishExam() {
+    // Lógica para finalizar a prova e redirecionar para a home
+  }
+
+  private loadSelections() {
+    // Carrega a seleção salva para a pergunta atual
+    this.selectedOption = this.selectedOptions[this.currentQuestionIndex] || null;
+  }
 }
